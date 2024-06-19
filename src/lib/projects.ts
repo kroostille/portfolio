ac535640328739d2ad1c0199ce1d8909ead2a035
Kroostille <vaimiti.dock@gmail.com>
2024-06-19 21:37:23 +0800

    feat: create `ProjectLayout`

---
+++ src/lib/projects.ts
@@ -0,0 +1,33 @@
+export type Project = {
+  id: "onix";
+  name: string;
+  scope: string;
+  year: number;
+  description: string;
+  team: { role: string; name: string }[];
+  tools: string;
+  link: string;
+  linkText: string;
+  media: string;
+  src: string;
+};
+
+export const projects: Project[] = [
+  {
+    id: "onix",
+    name: "ONIX Wallet Extension",
+    scope: "UI/UX Design",
+    year: 2024,
+    description:
+      "I have compared lots of crypto wallets last year and worked on designing one at my previous job. I wanted to design another one that will reflect the feelings I had when I was exploring the crypto ecosystem. I ended up with something darker and cyber.",
+    team: [
+      { role: "Designer", name: "Vaimiti Dock" },
+      { role: "Developer", name: "Kenji Lau" },
+    ],
+    tools: "Figma, After Effects",
+    link: "https://onix-7jbovx471-0xyami.vercel.app/index.html",
+    linkText: "Play demo",
+    media: "video",
+    src: "/assets/projects/onix/onix-intro.mp4",
+  },
+];
