afa9b7882c6b9fa4f4735ee02492c8dce57eb4f5
Kroostille <vaimiti.dock@gmail.com>
2024-06-20 17:31:06 +0800

    feat: add `Shadow Exchange` project

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -2,7 +2,7 @@ export type Project = {
   id: string;
   name: string;
   scope: string;
-  year: number;
+  year: string;
   description: string;
   team: { role: string; name: string }[];
   tools: string;
@@ -18,12 +18,12 @@ export const projects: Project[] = [
     id: "onix",
     name: "ONIX Wallet Extension",
     scope: "UI/UX Design",
-    year: 2024,
+    year: "2024",
     description:
       "I have compared lots of crypto wallets last year and worked on designing one at my previous job. I wanted to design another one that will reflect the feelings I had when I was exploring the crypto ecosystem. I ended up with something darker and cyber.",
     team: [
-      { role: "Designer", name: "Vaimiti Dock" },
-      { role: "Developer", name: "Kenji Lau" },
+      { role: "Designer", name: "Vaimiti DOCK" },
+      { role: "Developer", name: "Kenji LAU" },
     ],
     tools: "Figma, After Effects",
     link: "https://onix-7jbovx471-0xyami.vercel.app/index.html",
@@ -36,15 +36,34 @@ export const projects: Project[] = [
     id: "ichiba",
     name: "Ichiba Marketplace",
     scope: "Web Design",
-    year: 2024,
+    year: "2024",
     description:
       "Ichiba Market is a web design project of an e-commerce marketplace with a minimalist and consistent design. Through this project I wanted to experiment with CMS management and animations using Framer.",
-    team: [{ role: "Web Designer", name: "Vaimiti Dock" }],
+    team: [{ role: "Web Designer", name: "Vaimiti DOCK" }],
     tools: "Figma, Mid Journey, Framer",
     link: "https://ichiba-market.framer.website/",
     linkText: "Preview Framer template",
     mediaType: "image",
     mediaSrc: "/assets/projects/ichiba/ichiba-intro.jpg",
     mediaAlt: "Ichiba marketplace",
+  },
+  {
+    id: "shadow-exchange",
+    name: "Shadow NFT Exchange",
+    scope: "Product Design",
+    year: "2021-2023",
+    description:
+      "Shadow Exchange - old VIV3 - is an NFT marketplace with expanded and live analytics on the Flow blockchain. I have spent 2 years working at Shadow as Product designer. Working at the stage of a start-up, I have had to cover various disciplines from branding, UX research and design to project planning and management.",
+    team: [
+      { role: "Visual Identity", name: "Vaimiti DOCK" },
+      { role: "Web Designer", name: "Vaimiti DOCK" },
+      { role: "Animation", name: "Tasya ANGGITA RACHMANI" },
+    ],
+    tools: "Figma, Adobe Illustrator, AfterEffect, Rive, Linear, Notion",
+    link: "https://shadow.app/",
+    linkText: "Visit website",
+    mediaType: "image",
+    mediaSrc: "/assets/projects/shadow-exchange/shadow-exchange-intro.jpg",
+    mediaAlt: "Shadow NFT exchange",
   },
 ];
